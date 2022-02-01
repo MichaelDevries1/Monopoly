d9e1164ac29f98f7d31a52ed4c4f211af0dba2df
MichaelDevries <michael.devries16095@gmail.com>
2022-01-31 22:56:02 -0600

    Finally got the location objects to be correctly created. all other code is still commented out at this point. Minor adjustment to Locations.json. Added rent0 for all buildable locations as well as changed "Short Railroad" to "Short Line". Updated the utility class to include super.__init__

--- Scratch paper.py
+++ Scratch paper.py
@@ -2,208 +2,6 @@ import random
 import math
 import json
 
-
-def buildLocation():
-    locationNames = {}
-    cardType = ''
-    locationFile = open('C:\\Users\\compu\\Dropbox\\selfStudy\\Monopoly\\Monopoly\\Locations.json')
-    data = json.load(locationFile)
-
-    for i in data['locations']:
-        if data['locations'][i]['proptype'] == "go":
-             locationNames[i] = Go(
-                propType=data['locations'][i]['proptype'],
-                cardName=data['locations'][i]['name'],
-            )
-        elif cardType == "buildable":
-            return Buildable(
-                pType="buildable",
-                pName="Mediteranian Avenue",
-                pCost=60,
-                pRent=2,
-                pRent1=10,
-                pRent4=160,
-                pRentHotel=250,
-                pBuildingCost=50
-            )
-        elif cardType == 'railroad':
-            return Railroad(
-                pType='railroad',
-                pName='Reading Railroad',
-                pCost=200,
-                pRent=25
-            )
-        elif cardType == 'simpleLocation':
-            return SimpleLocation(
-                pType='simpleLocation',
-                pName='Free Parking'
-            )
-        elif cardType == 'utility':
-            return Utility(
-                pType='utility',
-                pName='Electric Company',
-                pCost=150
-            )
-
-    return locationNames
-
-
-name = {}
-f = open('C:\\Users\\compu\\Dropbox\\selfStudy\\Monopoly\\Monopoly\\Locations.json')
-data = json.load(f)
-
-for i in data['locations']:
-    name[i] = buildLocation(data['locations'][i])
-    print(name[i])
-
-f.close()
-'''
-def advancetogo():
-    print('Advance to Go. (Collect $200) \n\n\n\n\n')
-'''
-
-
-
-'''
-# Setting Variables
-# Name of all locations on the board
-name = {
-    0: "Go",
-    1: 'Mediterranean Avenue',
-    2: 'Community Chest',
-    3: 'Baltic Avenue',
-    4: 'Income Tax',
-    5: 'Reading Railroad',
-    6: 'Oriental Avenue',
-    7: 'Chance',
-    8: 'Vermont Avenue',
-    9: 'Connecticut Avenue',
-    10: 'Just Visiting',
-    11: 'St. Charles Place',
-    12: 'Electric Company',
-    13: 'States Avenue',
-    14: 'Virginia Avenue',
-    15: 'Pennsylvania Railroad',
-    16: 'St. James Place',
-    17: 'Community Chest',
-    18: 'Tennessee Avenue',
-    19: 'New York Avenue',
-    20: 'Free Parking',
-    21: 'Kentucky Avenue',
-    22: 'Chance',
-    23: 'Indiana Avenue',
-    24: 'Illinois Avenue',
-    25: 'B&O Railroad',
-    26: 'Atlantic Avenue',
-    27: 'Ventnor Avenue',
-    28: 'Water Works',
-    29: 'Marvin Gardens',
-    30: 'Go To Jail',
-    31: 'Pacific Avenue',
-    32: 'North Carolina Avenue',
-    33: 'Community Chest',
-    34: 'Pennsylvania Avenue',
-    35: 'Short Line',
-    36: 'Chance',
-    37: 'Park Place',
-    38: 'Luxury Tax',
-    39: 'Boardwalk',
-    40: 'In Jail'
-}
-# All Chance cards
-chance = {
-    1: 'Advance to Boardwalk',
-    2: 'Advance to Go',
-    3: 'Advance to Illinois Avenue. If you pass Go, collect $200',
-    4: 'Advance to St. Charles Place. If you pass Go, collect $200',
-    5: 'Advance to the nearest railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental'
-       'to which they are otherwise entitled.',
-    6: 'Advance to the nearest railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental'
-       'to which they are otherwise entitled.',
-    7: 'Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner'
-       'a total of ten times amount thrown.',
-    8: 'Bank pays you dividend of $50',
-    9: 'Get Out of Jail Free',
-    10: 'Go back 3 spaces',
-    11: 'Go to Jail. Go directly to Jail, do not pass Go, do not collect $200',
-    12: 'Make general repairs on all your property. For each house pay $25. For each hotel pay $100.',
-    13: 'Speeding fine $15',
-    14: 'Take a trip to Reading Railroad. If you pass Go, collect $200',
-    15: 'You have been elected Chairman of the Board. Pay each player $50',
-    16: 'Your building loan matures. Collect $150'
-}
-# All Community Chest Cards
-com_chest = {
-    1: 'advancetogo()',
-    2: 'Bank error in your favor. Collect $200',
-    3: "Doctor's fee. Pay $50",
-    4: 'From sale of stock you get $50',
-    5: 'Get Out of Jail Free',
-    6: 'Go to Jail. Go directly to Jail, do not pass Go, do not collect $200',
-    7: 'Holiday fund matures. Receive $100',
-    8: 'Income tax refund. Collect $20',
-    9: 'It is your birthday. Collect $10 from every player.',
-    10: 'Life insurance matures. Collect $100.',
-    11: 'Pay hospital fees of $100',
-    12: 'Pay school fees of $50',
-    13: 'Receive $25 consultancy fee.',
-    14: 'You are assessed for street repair. $40 per house. $115 per hotel.',
-    15: 'You have won second prize in a beauty contest. Collect $10',
-    16: 'You inherit $100.'
-}
-# Dice variables
-dice1 = -1
-dice2 = -1
-double_count = 0
-# Player Location on the board
-location = 0
-# Turn number this game
-turn = 0
-cardType = "Go"
-
-while turn < 200:
-    # Increase turn count and display
-    turn += 1
-    print("Turn #: ", turn)
-
-    # Roll the 2 dice, add them together and display the results
-    dice1 = random.randint(1, 6)
-    dice2 = random.randint(1, 6)
-    dice_sum = dice1 + dice2
-    print("Dice 1: ", dice1, " Dice 2: ", dice2, " Dice sum: ", dice_sum)
-
-    # Monitor number of doubles rolled in a row
-    if dice1 == dice2:
-        double_count += 1
-        print('Doubles thrown: ', double_count)
-    else:
-        double_count = 0
-
-    # Figure out new location of player
-    location += dice_sum
-    # Check if player goes to jail or returns to Go naturally
-    if double_count == 3 or location == 30:
-        print('WENT TO JAIL \n\n\n')
-        location = 40
-    elif location > 39:
-        location -= 40
-
-    # Check if player landed on chance and draw card.
-    if location == 7 or location == 22 or location == 36:
-        draw_chance = chance[random.randint(1, len(chance))]
-        print('Take a chance card! You drew: ', draw_chance, '\n\n\n')
-
-    # Check if player landed on community chest and draw a card.
-    if location == 2 or location == 17 or location == 33:
-        draw_comm = eval(com_chest[1])
-        # draw_comm = com_chest[random.randint(1, len(com_chest))]
-        # print('Take a community chest card! You drew: ', draw_comm, '\n\n\n')
-
-    # Display current location
-    print('Location: ', name[location])
-    print('')
-'''
-
 class SimpleLocation:
     type = ''
     name = ''
@@ -217,6 +15,9 @@ class SimpleLocation:
 
     def getName(self):
         return self.name
+
+    def getInfo(self):
+        return [self.type, self.name]
 
 
 class Go(SimpleLocation):
@@ -388,9 +189,7 @@ class Utility(Property):
     level = 0
 
     def __init__(self, pType, pName, pCost):
-        self.type = pType
-        self.name = pName
-        self.cost = pCost
+        super.__init__(self, pType, pName, pCost)
         self.mortgageValue = self.cost / 2
         self.unmortgageCost = int(math.ceil(self.mortgageValue * 1.1))
 
@@ -439,3 +238,204 @@ class Utility(Property):
     def getInfo(self):
         return [self.type, self.name, self.cost, self.rent1, self.rent2, self.mortgageValue, self.unmortgageCost,
                 self.isMortgaged, self.owner, self.level]
+
+
+def buildLocation():
+    locationNames = {}
+    locationFile = open('C:\\Users\\compu\\Dropbox\\selfStudy\\Monopoly\\Monopoly\\Locations.json')
+    data = json.load(locationFile)
+    x = 0
+
+    for i in data['locations']:
+        cardType = data['locations'][x]['proptype']
+        if cardType == "go":
+            locationNames[x] = Go(
+                propType='go',
+                cardName=data['locations'][x]['name'],
+            )
+        elif cardType == "buildable":
+            locationNames[x] = Buildable(
+                pType='buildable',
+                pName=data['locations'][x]['name'],
+                pCost=data['locations'][x]['cost'],
+                pRent=data['locations'][x]['rent0'],
+                pRent1=data['locations'][x]['rent1'],
+                pRent4=data['locations'][x]['rent4'],
+                pRentHotel=data['locations'][x]['rentHotel'],
+                pBuildingCost=data['locations'][x]['buildingCost']
+            )
+        elif cardType == 'tax':
+            locationNames[x] = Tax(
+                pType='tax',
+                pName=data['locations'][x]['name'],
+                pTax=data['locations'][x]['tax']
+            )
+        elif cardType == 'railroad':
+            locationNames[x] = Railroad(
+                pType='railroad',
+                pName=data['locations'][x]['name'],
+                pCost=data['locations'][x]['cost']
+            )
+        elif cardType == 'simpleLocation':
+            locationNames[x] = SimpleLocation(
+                pType='simpleLocation',
+                pName=data['locations'][x]['name']
+            )
+        elif cardType == 'utility':
+            locationNames[x] = Utility(
+                pType='utility',
+                pName=data['locations'][x]['name'],
+                pCost=data['locations'][x]['cost']
+            )
+        print(str(x), ': ', locationNames.get(x).getName())
+        x += 1
+    locationFile.close()
+    return locationNames
+
+
+name = buildLocation()
+'''
+def advancetogo():
+    print('Advance to Go. (Collect $200) \n\n\n\n\n')
+
+# Setting Variables
+# Name of all locations on the board
+name = {
+    0: "Go",
+    1: 'Mediterranean Avenue',
+    2: 'Community Chest',
+    3: 'Baltic Avenue',
+    4: 'Income Tax',
+    5: 'Reading Railroad',
+    6: 'Oriental Avenue',
+    7: 'Chance',
+    8: 'Vermont Avenue',
+    9: 'Connecticut Avenue',
+    10: 'Just Visiting',
+    11: 'St. Charles Place',
+    12: 'Electric Company',
+    13: 'States Avenue',
+    14: 'Virginia Avenue',
+    15: 'Pennsylvania Railroad',
+    16: 'St. James Place',
+    17: 'Community Chest',
+    18: 'Tennessee Avenue',
+    19: 'New York Avenue',
+    20: 'Free Parking',
+    21: 'Kentucky Avenue',
+    22: 'Chance',
+    23: 'Indiana Avenue',
+    24: 'Illinois Avenue',
+    25: 'B&O Railroad',
+    26: 'Atlantic Avenue',
+    27: 'Ventnor Avenue',
+    28: 'Water Works',
+    29: 'Marvin Gardens',
+    30: 'Go To Jail',
+    31: 'Pacific Avenue',
+    32: 'North Carolina Avenue',
+    33: 'Community Chest',
+    34: 'Pennsylvania Avenue',
+    35: 'Short Line',
+    36: 'Chance',
+    37: 'Park Place',
+    38: 'Luxury Tax',
+    39: 'Boardwalk',
+    40: 'In Jail'
+}
+# All Chance cards
+chance = {
+    1: 'Advance to Boardwalk',
+    2: 'Advance to Go',
+    3: 'Advance to Illinois Avenue. If you pass Go, collect $200',
+    4: 'Advance to St. Charles Place. If you pass Go, collect $200',
+    5: 'Advance to the nearest railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental'
+       'to which they are otherwise entitled.',
+    6: 'Advance to the nearest railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental'
+       'to which they are otherwise entitled.',
+    7: 'Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner'
+       'a total of ten times amount thrown.',
+    8: 'Bank pays you dividend of $50',
+    9: 'Get Out of Jail Free',
+    10: 'Go back 3 spaces',
+    11: 'Go to Jail. Go directly to Jail, do not pass Go, do not collect $200',
+    12: 'Make general repairs on all your property. For each house pay $25. For each hotel pay $100.',
+    13: 'Speeding fine $15',
+    14: 'Take a trip to Reading Railroad. If you pass Go, collect $200',
+    15: 'You have been elected Chairman of the Board. Pay each player $50',
+    16: 'Your building loan matures. Collect $150'
+}
+# All Community Chest Cards
+com_chest = {
+    1: 'advancetogo()',
+    2: 'Bank error in your favor. Collect $200',
+    3: "Doctor's fee. Pay $50",
+    4: 'From sale of stock you get $50',
+    5: 'Get Out of Jail Free',
+    6: 'Go to Jail. Go directly to Jail, do not pass Go, do not collect $200',
+    7: 'Holiday fund matures. Receive $100',
+    8: 'Income tax refund. Collect $20',
+    9: 'It is your birthday. Collect $10 from every player.',
+    10: 'Life insurance matures. Collect $100.',
+    11: 'Pay hospital fees of $100',
+    12: 'Pay school fees of $50',
+    13: 'Receive $25 consultancy fee.',
+    14: 'You are assessed for street repair. $40 per house. $115 per hotel.',
+    15: 'You have won second prize in a beauty contest. Collect $10',
+    16: 'You inherit $100.'
+}
+# Dice variables
+dice1 = -1
+dice2 = -1
+double_count = 0
+# Player Location on the board
+location = 0
+# Turn number this game
+turn = 0
+cardType = "Go"
+
+while turn < 200:
+    # Increase turn count and display
+    turn += 1
+    print("Turn #: ", turn)
+
+    # Roll the 2 dice, add them together and display the results
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    dice_sum = dice1 + dice2
+    print("Dice 1: ", dice1, " Dice 2: ", dice2, " Dice sum: ", dice_sum)
+
+    # Monitor number of doubles rolled in a row
+    if dice1 == dice2:
+        double_count += 1
+        print('Doubles thrown: ', double_count)
+    else:
+        double_count = 0
+
+    # Figure out new location of player
+    location += dice_sum
+    # Check if player goes to jail or returns to Go naturally
+    if double_count == 3 or location == 30:
+        print('WENT TO JAIL \n\n\n')
+        location = 40
+    elif location > 39:
+        location -= 40
+
+    # Check if player landed on chance and draw card.
+    if location == 7 or location == 22 or location == 36:
+        draw_chance = chance[random.randint(1, len(chance))]
+        print('Take a chance card! You drew: ', draw_chance, '\n\n\n')
+
+    # Check if player landed on community chest and draw a card.
+    if location == 2 or location == 17 or location == 33:
+        draw_comm = eval(com_chest[1])
+        # draw_comm = com_chest[random.randint(1, len(com_chest))]
+        # print('Take a community chest card! You drew: ', draw_comm, '\n\n\n')
+
+    # Display current location
+    print('Location: ', name[location])
+    print('')
+'''
+
+
+
